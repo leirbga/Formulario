eee6f44910dd75d470f507557c8cd4c458d0a410
DonOviedo <joseoviedocolina@gmail.com>
2026-05-18 23:11:05 -0400

    formulario corregido

--- index.js
+++ index.js
@@ -1,118 +1,121 @@
-const NOMBRE_USUARIO_REGEX = /^[a-zA-Z0-9_-]{4,8}$/; 
-const EMAIL_REGEX =/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const NOMBRE_USUARIO_REGEX = /^[a-zA-Z0-9_-]{4,8}$/;
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const NUMERO_TELEFONO_REGEX = /^[\+]?[(+]?[\d\s\-\(\)]{8,}$/;
-const CONTRASEÑA_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; 
-const CONFIRMAR_CONTRASEÑA_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; 
-const paises = document.querySelector('#countries');
-const ponerNombre = document.querySelector('#username');
-const ponerEmail = document.querySelector('#email');
-const selectPais = document.querySelector('#countries');
-const codigo = document.querySelector('#phone-code');
-const telefono = document.querySelector('#phone');
-const contra = document.querySelector('#password');
-const confirContra = document.querySelector('#confirm-password');
-const registrar = document.querySelector('.abilitar');
-const registrar2 = document.querySelector('#form-btn');
-const registrar3 = document.querySelector('#registrado');
-const registrar4 = document.querySelector('.contenido2')
+const CONTRASEÑA_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const CONFIRMAR_CONTRASEÑA_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const paises = document.querySelector("#countries");
+const introducirNombre = document.querySelector("#username");
+const introducirEmail = document.querySelector("#email");
+const codigoTelefono = document.querySelector("#phone-code");
+const telefono = document.querySelector("#phone");
+const contrasena = document.querySelector("#password");
+const confirmarContrasena = document.querySelector("#confirm-password");
+const habilitarBoton = document.querySelector(".habilitar-btn");
+const botonRegistrar = document.querySelector("#form-btn");
+const activarBoton = document.querySelector("#registrado");
+const datosUsuario = document.querySelector(".contenido2");
 
-let usernameValidacion = false;
+let nombreValidacion = false;
 let emailValidacion = false;
-let telfValidacion = false;
-let passValidacion = false;
-let passValidacion2 = false;
-let paisesVali = false;
+let telfonoValidacion = false;
+let constrasenaValidacion = false;
+let compararContrasena = false;
+let paisesValidacion = false;
 
-[ ... countries].forEach(opciones =>{
-    opciones.innerHTML = opciones.innerHTML.split('(')[0];
+[...countries].forEach((opciones) => {
+  opciones.innerHTML = opciones.innerHTML.split("(")[0];
 });
 
-const validaciones = (evento, validaciones, element) =>{
-    const imformacion = evento.target.parentElement.children[1];
-    const opcionesValidas = usernameValidacion && emailValidacion && telfValidacion && passValidacion && passValidacion2 && paisesVali;
-    if(opcionesValidas){
-        registrar.classList.remove('abilitar');
-    }
-    else{
-        registrar.classList.add('abilitar');
-    }
+const validaciones = (evento, validaciones, element) => {
+  const informacion = evento.target.parentElement.children[1];
+  const opcionesValidas =
+    nombreValidacion &&
+    emailValidacion &&
+    telfonoValidacion &&
+    constrasenaValidacion &&
+    compararContrasena &&
+    paisesValidacion;
+  if (opcionesValidas) {
+    habilitarBoton.classList.remove("habilitar-btn");
+  } else {
+    habilitarBoton.classList.add("habilitar-btn");
+  }
 
-    if (validaciones){
-        element.classList.add('repuestaCorrecta');
-        element.classList.remove('repuestaErronea');
-        imformacion.classList.remove('mostarinfo');
-        
-    }else{
-        element.classList.add('repuestaErronea');
-        element.classList.remove('repuestaCorrecta');
-        imformacion.classList.add('mostarinfo')
-    }
+  if (validaciones) {
+    element.classList.add("repuestaCorrecta");
+    element.classList.remove("repuestaErronea");
+    informacion.classList.remove("mostarInformacion");
+  } else {
+    element.classList.add("repuestaErronea");
+    element.classList.remove("repuestaCorrecta");
+    informacion.classList.add("mostarInformacion");
+  }
 };
 
-ponerNombre.addEventListener('input', evento =>{
-  usernameValidacion = NOMBRE_USUARIO_REGEX.test(evento.target.value);
-    validaciones(evento, usernameValidacion, ponerNombre);
+introducirNombre.addEventListener("input", (evento) => {
+  nombreValidacion = NOMBRE_USUARIO_REGEX.test(evento.target.value);
+  validaciones(evento, nombreValidacion, introducirNombre);
 });
 
-
-ponerEmail.addEventListener('input', evento =>{
- emailValidacion = EMAIL_REGEX.test(evento.target.value);
-  validaciones(evento, emailValidacion, ponerEmail);
+introducirEmail.addEventListener("input", (evento) => {
+  emailValidacion = EMAIL_REGEX.test(evento.target.value);
+  validaciones(evento, emailValidacion, introducirEmail);
 });
 
-selectPais.addEventListener('input', evento =>{
- const paiselect = [... evento.target.children].find(option => option.selected);
- paisesVali = true;
- codigo.innerHTML = `+${selectPais.value}`;
- selectPais.classList.add('repuestaCorrecta');
- codigo.classList.add('repuestaCorrecta');
+paises.addEventListener("input", (evento) => {
+  const paisSeleccionado = [...evento.target.children].find(
+    (option) => option.selected,
+  );
+  paisesValidacion = paisSeleccionado.value == "" ? false : true;
+  //  console.log(validarPais);
+  codigoTelefono.innerHTML = `+${paises.value}`;
+  paises.classList.add("repuestaCorrecta");
+  codigoTelefono.classList.add("repuestaCorrecta");
 });
 
-telefono.addEventListener('input', evento =>{
- telfValidacion = NUMERO_TELEFONO_REGEX.test(evento.target.value);
-  validaciones(evento, telfValidacion, telefono);
- const imformacion = evento.target.parentElement.parentElement.children[1];
-  if (telfValidacion){
-        telefono.classList.add('repuestaCorrecta');
-        telefono.classList.remove('repuestaErronea');
-        imformacion.classList.remove('mostarinfo')
-    }else{
-        telefono.classList.add('repuestaErronea');
-        telefono.classList.remove('repuestaCorrecta');
-        imformacion.classList.add('mostarinfo')
-    }
+telefono.addEventListener("input", (evento) => {
+  telfonoValidacion = NUMERO_TELEFONO_REGEX.test(evento.target.value);
+  validaciones(evento, telfonoValidacion, telefono);
+  const informacion = evento.target.parentElement.parentElement.children[1];
+  if (telfonoValidacion) {
+    telefono.classList.add("repuestaCorrecta");
+    telefono.classList.remove("repuestaErronea");
+    informacion.classList.remove("mostarInformacion");
+  } else {
+    telefono.classList.add("repuestaErronea");
+    telefono.classList.remove("repuestaCorrecta");
+    informacion.classList.add("mostarInformacion");
+  }
 });
 
-
-contra.addEventListener('input', evento =>{
- passValidacion = CONTRASEÑA_REGEX.test(evento.target.value);
- validaciones(evento, passValidacion, contra);
+contrasena.addEventListener("input", (evento) => {
+  constrasenaValidacion = CONTRASEÑA_REGEX.test(evento.target.value);
+  validaciones(evento, constrasenaValidacion, contrasena);
 });
 
-confirContra.addEventListener('input', evento =>{
- passValidacion2 = contra.value == evento.target.value;
-    validaciones(evento, passValidacion2, confirContra);
- 
+confirmarContrasena.addEventListener("input", (evento) => {
+  compararContrasena = contrasena.value == evento.target.value;
+  validaciones(evento, compararContrasena, confirmarContrasena);
 });
 
-
-registrar2.addEventListener('click', evento =>{
-    evento.preventDefault();
-    registrar3.style.display = 'flex';
-    const respuestas = {
-        Username: ponerNombre.value,
-        Email: ponerEmail.value,
-        Phone: `${codigo.innerHTML} ${telefono.value}`,
-    }
-    registrar4.innerHTML= `
+botonRegistrar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  activarBoton.style.display = "flex";
+  const respuestas = {
+    Username: introducirNombre.value,
+    Email: introducirEmail.value,
+    Phone: `${codigoTelefono.innerHTML} ${telefono.value}`,
+  };
+  datosUsuario.innerHTML = `
         <h1>Felicidadees te has registrado con Exito!</h1>
         <p>Tus datos registrados:<br>
         Nombre: ${respuestas.Username} <br>
         Email: ${respuestas.Email} <br>
-        Telefono: ${codigo.innerHTML} ${telefono.value}
+        Teléfono: ${respuestas.Phone}
         </p>
     `;
-    
-    console.log(respuestas);
-});
 
+  console.log(respuestas);
+});
